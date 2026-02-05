bfaaa73f798c429e7702af0dabd5ca59f27803c5
cherry pick <limhojin956@naver.com>
2026-02-05 11:19:17 +0900

    fix : API명세서 검토결과 LV2 코드 수정(id에서 editor로)

--- src/main/java/com/example/project/repository/ScheduleRepository.java
+++ src/main/java/com/example/project/repository/ScheduleRepository.java
@@ -3,5 +3,12 @@ package com.example.project.repository;
 import com.example.project.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAllByEditorOrderByUpdateDateDesc(String editor);
+
+    List<Schedule> findAllByOrderByUpdateDateDesc();
+
 }
